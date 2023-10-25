1a0f7511960893c56faf89e85a630ebd4e4193d3
wakaba283 <114409214+wakaba283@users.noreply.github.com>
2023-10-25 23:59:29 +0900

    changed readthedoc theme

--- docs/conf.py
+++ docs/conf.py
@@ -25,5 +25,11 @@ language = 'ja'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
+
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
